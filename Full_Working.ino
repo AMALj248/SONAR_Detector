6692a2bfd672e38fe89219bf687cc9290e7d3d13
Amal Jogy <54910604+AMALj248@users.noreply.github.com>
2020-09-13 11:43:51 +0530

    Update Full_Working.ino
    All commands are commented and explained.

--- Full_Working.ino
+++ Full_Working.ino
@@ -1,3 +1,4 @@
+//Code By AJ
 #include <SegmentDisplay.h>
 #include<Servo.h>
 #include <NewPing.h>
@@ -10,11 +11,13 @@ int angle =0;
 
  // NewPing setup of pins and maximum distance
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+// Setup the & segment Display with the correct pin order 
 SegmentDisplay segmentDisplay(2, 3, 4, 5, 6, 7, 8, 9);
+//Setup for the Servo 
 Servo servo_test;
 
 void setup() {
-  // put your setup code here, to run once:
+  // Configuring Pins for output for 7 segment
    pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
@@ -24,28 +27,42 @@ void setup() {
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
 
+ //Configuring Pin for LED
   pinMode(led, OUTPUT);
+
+  //Configuring Pin for Servo
   servo_test.attach(srv);
+
+  //Configuring HC-SR04  Serial 
   Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-  // put your main code here, to run repeatedly:
+// To turn the Servo in Postive Direction
 for(angle=0;angle<180;angle++)
-{servo_test.write(angle);
+{
+ // T move the servo 
+servo_test.write(angle);
 delay(5);
+
+// To Calculate Distance 
 unsigned int distance = sonar.ping_cm();
+
+// To Print Distance to Serial Monitor 
    Serial.print(distance);
    Serial.println("cm");
-   
+
+// Danger Proximity Check   
    if(distance<10)
    {
+    //Display the Distance to 7 segment Display 
     segmentDisplay.displayHex(distance , false);
+    //To turn LED ON 
     digitalWrite(led , HIGH);
     delay(20);
+    //To turn LED OFF 
     digitalWrite(led , LOW);
     
     }
@@ -54,6 +71,7 @@ unsigned int distance = sonar.ping_cm();
 
 delay(1000);
 
+// To turn the Servo in Negative Direction
 for(angle=180;angle>1;angle--)
 {servo_test.write(angle);
 delay(5);
